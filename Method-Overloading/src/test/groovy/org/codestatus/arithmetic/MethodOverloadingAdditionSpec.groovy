a745bc9cb7cdfbb4660355fedd339d324a9feef6
Sharon Wilson <vwsharon@live.com>
2022-01-12 19:11:23 +0530

    Update MethodOverloadingAdditionSpec.groovy

--- Method-Overloading/src/test/groovy/org/codestatus/arithmetic/MethodOverloadingAdditionSpec.groovy
+++ Method-Overloading/src/test/groovy/org/codestatus/arithmetic/MethodOverloadingAdditionSpec.groovy
@@ -2,33 +2,33 @@ package org.codestatus.arithmetic
 
 import spock.lang.Specification
 
-class MethodOverloadingAdditionSpec extends Specification{
+class MethodOverloadingAdditionSpec extends Specification {
 
 
     def MethodOverloadingAddition = new MethodOverloadingAddition()
 
-    def 'addIntegerTest_01' (){
+    def 'addIntegerTest_01'() {
 
         expect:
 
         MethodOverloadingAddition.addNumbers(20, 05) == 25
     }
 
-    def 'addIntegerTest_02' (){
+    def 'addIntegerTest_02'() {
 
         expect:
 
         !(MethodOverloadingAddition.addNumbers(20, 05) == 30)
     }
 
-    def 'addIntegerTest_03' (){
+    def 'addIntegerTest_03'() {
 
         expect:
 
         MethodOverloadingAddition.addNumbers(45, 05) != 60
     }
 
-    def 'addDoubleTest_01' (){
+    def 'addDoubleTest_01'() {
 
         expect:
 
@@ -42,7 +42,7 @@ class MethodOverloadingAdditionSpec extends Specification{
         MethodOverloadingAddition.addNumbers(34, 50.5) == 84.5
     }
 
-    def 'addNumbersTest_01'(){
+    def 'addNumbersTest_01'() {
 
         expect:
 
@@ -50,8 +50,8 @@ class MethodOverloadingAdditionSpec extends Specification{
 
         where:
 
-        num1   << [20, 45.90, 99]
-        num2   << [45, 35.75, 45.99]
+        num1 << [20, 45.90, 99]
+        num2 << [45, 35.75, 45.99]
         result << [65, 81.65, 144.99]
     }
 
